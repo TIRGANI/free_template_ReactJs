3868723619a0ddcff84aaef27ff5e58f73a2a432
tirgani <tirgani.badreddine96@gmail.com>
2022-08-03 11:33:18 +0100

    ligne de sujet (essayer de la garder sous 50 caractères)
    description multiligne du commit
    ajouter tous les détails que vous voulez

--- src/views/vue_entreprise_profileCandidat/index.js
+++ src/views/vue_entreprise_profileCandidat/index.js
@@ -38,6 +38,26 @@ import { strengthColor, strengthIndicator } from 'utils/password-strength';
 // assets
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+//table
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+//function table
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9)
+];
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
@@ -74,170 +94,51 @@ const Entreprise_profileCandidat = () => (
             {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
                 <form noValidate>
                     <Grid container spacing={2} alignItems="center" justifyContent="center">
-                        <Grid item xs zeroMinWidth>
-                            <TextField multiline fullWidth label="Titre Offre" margin="normal" name="fname" type="text" defaultValue="" />
-                        </Grid>
                         <Grid item xs={12}>
-                            <Grid container spacing={2}>
-                                <Grid item lg={6} md={6} sm={6} xs={12}>
-                                    <FormControl fullWidth>
-                                        <InputLabel id="demo-simple-select-label">Status de L'offre</InputLabel>
-                                        <Select
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            label="Status"
-                                            onChange={handleChange}
+                            <Grid item>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Dessert (100g serving)</TableCell>
+                                                <TableCell align="right">Calories</TableCell>
+                                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                                    <TableCell component="th" scope="row">
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="right">{row.calories}</TableCell>
+                                                    <TableCell align="right">{row.fat}</TableCell>
+                                                    <TableCell align="right">{row.carbs}</TableCell>
+                                                    <TableCell align="right">{row.protein}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
+                            <Grid item tem xs={2}>
+                                <Box sx={{ mt: 2 }}>
+                                    <AnimateButton>
+                                        <Button
+                                            disableElevation
+                                            disabled={isSubmitting}
+                                            fullWidth
+                                            size="large"
+                                            type="submit"
+                                            variant="contained"
+                                            color="secondary"
                                         >
-                                            <MenuItem value="active">Active</MenuItem>
-                                            <MenuItem value="enPause">En Pause</MenuItem>
-                                            <MenuItem value="pourvue">Pourvue</MenuItem>
-                                            <MenuItem value="pourvue">Validée</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-
-                                <Grid item lg={6} md={6} sm={6} xs={12}>
-                                    <TextField multiline fullWidth label="Points Incontournable" name="lname" type="text" defaultValue="" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Description du poste"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Profile Rechercher"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Rémunération"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Avantages & Contexte"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField multiline fullWidth label="#Tags" margin="normal" name="lname" type="text" defaultValue="" />
-                                </Grid>
-                                <Grid item tem xs={2}>
-                                    <Box sx={{ mt: 2 }}>
-                                        <AnimateButton>
-                                            <Button
-                                                disableElevation
-                                                disabled={isSubmitting}
-                                                fullWidth
-                                                size="large"
-                                                variant="contained"
-                                                color="primary"
-                                            >
-                                                + Valideur
-                                            </Button>
-                                        </AnimateButton>
-                                    </Box>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField multiline fullWidth label="Roles" margin="normal" name="lname" type="text" defaultValue="" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Adresse Mail"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Login Valideur"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Password Valideur"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Changer de mot de pass a la premier connxion"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        fullWidth
-                                        label="Etat des validations"
-                                        margin="normal"
-                                        name="lname"
-                                        type="text"
-                                        defaultValue=""
-                                    />
-                                </Grid>
-
-                                <Grid item tem xs={2}>
-                                    <Box sx={{ mt: 2 }}>
-                                        <AnimateButton>
-                                            <Button
-                                                disableElevation
-                                                disabled={isSubmitting}
-                                                fullWidth
-                                                size="large"
-                                                type="submit"
-                                                variant="contained"
-                                                color="secondary"
-                                            >
-                                                PDF
-                                            </Button>
-                                        </AnimateButton>
-                                    </Box>
-                                </Grid>
+                                            PDF
+                                        </Button>
+                                    </AnimateButton>
+                                </Box>
                             </Grid>
                         </Grid>
                     </Grid>
